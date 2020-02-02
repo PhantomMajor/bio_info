2b71d4094014b61d1ed88c85e82d17a507bc64bc
Biotech-UG-IITKgp <1udayaditya@gmail.com>
2020-02-02 22:27:23 +0530

    Added working code and files for experiment. Files modified: que2.c . All others are obtained by executing the a.out file.

--- expt2/que2.c
+++ expt2/que2.c
@@ -14,7 +14,12 @@
 int main(void)
 {
 	FILE *fpt1; // File pointer for file to be opened (1ASY.pdb)
-	FILE *fpt2; // File pointer for output file (out.pdb)
+	FILE *fpt2; // File pointer for output interacting protein file (out1_prot.pdb)
+	FILE *fpt3; // File pointer for output interacting rna file (out2_rna.pdb)
+	FILE *fpt4; // File pointer for output interacting pairs (out3_interact.txt)
+	FILE *fpt_pAll; // File pointer for all the proteins
+	FILE *fpt_rAll; // File pointer for all the rna
+
 
 	// File info [Magic Numbers]
 	int num_cols_in_file = 80; // Maximum 80 columns are in the file
@@ -25,21 +30,21 @@ int main(void)
 	int residue_no_len = 4; // Max length of residue number
 	int coord_len = 8; // Max length of coordinates
 
-	double distance = 0; // To calculate distance
-
 	char line[num_cols_in_file]; // to read and store the lines in the file
 	char word[max_word_length]; // to store the word in the line
 
 	char r_array[20000][num_cols_in_file]; // array to store rna atoms
 	char p_array[20000][num_cols_in_file]; // array to store prot atoms
 
-	int r_counter = 0;
-	int p_counter = 0;
+	int r_counter = 0; // rna counter
+	int p_counter = 0; // protein counter
 
 	int i, j; // counter for loops
 
-	fpt1 = fopen("1asy.pdb", "r");
-	fpt2 = fopen("out.pdb", "w");
+	fpt1 = fopen("1asy.pdb", "r");  // input file
+	fpt2 = fopen("out1_prot.pdb", "w"); // output the proteins in the file
+	fpt3 = fopen("out2_rna.pdb", "w"); // output the rna in the file
+	fpt4 = fopen("out3_interact.txt", "w"); // output the interacting pairs
 
 
 	/* -- ID ALL RNA AND PROT ATOMS -- */
@@ -69,11 +74,45 @@ int main(void)
 		}
 	}
 
+	/*	
+	// -- FOR PRINTING ALL PROT AND RNA FILES -- 
+	//---------- For Debugging Only -------------
+	fpt_rAll = fopen("rnaAll.pdb", "w");
+	fpt_pAll = fopen("protAll.pdb", "w");
+
+	// A. Printing Protein file
+	for (i = 0; i < p_counter; i++) // looping through the rows of protein array
+	{
+		fprintf(fpt_pAll, "%s\n", p_array[i]);	
+	}
+	// B. Printing RNA file
+	for (i = 0; i < r_counter; i++) // looping through the rows of protein array
+	{
+		fprintf(fpt_rAll, "%s\n", r_array[i]);	
+	}
+
+	// closing the files
+	fclose(fpt_pAll);
+	fclose(fpt_rAll);
+	//------------------- END ---------------------
+	*/
+
 	int temp; // counter for reading coordinates
 	char coord[8]; // holder array for coordinate
-	int cord_xo, cord_yo, cord_zo; // coordinate holder for outer loop
-	int cord_xi, cord_yi, cord_zi; // coordinate holder for inner loop
+	float cord_xo, cord_yo, cord_zo; // coordinate holder for outer loop
+	float cord_xi, cord_yi, cord_zi; // coordinate holder for inner loop
 
+	int count_interact = 0; // counter for interacting atoms
+	double distance = 0; // To calculate distance
+
+	// to store variables to put in out4_interact.txt
+	char atom_number_p[5], atom_name_p[3], atom_number_r[5], atom_name_r[3];
+	int int_atom_number_p, int_atom_number_r;
+	char residue_name_p[3], residue_name_r[3];
+	char residue_num_p[4], residue_num_r[4];
+	int int_residue_num_p, int_residue_num_r;
+
+	/* -- FOR INTERACTING ATOMS FILE -- */
 	for (i = 0; i < r_counter; i++) // outer loop on RNA array
 	{
 		// RNA atom coordinates
@@ -101,30 +140,87 @@ int main(void)
 			// PROT atom coordinates
 			for ( temp = 0; temp < 8; temp++)
 			{
-				coord[temp] = p_array[i][temp+30]; // from the i-th line of p_array, read line
+				coord[temp] = p_array[j][temp+30]; // from the i-th line of p_array, read line
 			}
 		    coord[8]='\0';
 		    cord_xi=atof(coord);
 		    for ( temp = 0; temp < 8; temp++)
 			{
-				coord[temp] = p_array[i][temp+38];
+				coord[temp] = p_array[j][temp+38];
 			}
 		    coord[8]='\0';
 		    cord_yi=atof(coord);
 		    for ( temp = 0; temp < 8; temp++)
 			{
-				coord[temp] = p_array[i][temp+46];
+				coord[temp] = p_array[j][temp+46];
 			}
 		    coord[8]='\0';
 		    cord_zi=atof(coord);
 
 			// compare distances
+		    distance = sqrt( 
+		    	pow((cord_xo - cord_xi), 2) + 
+		    	pow((cord_yo - cord_yi), 2) + 
+		    	pow((cord_zo - cord_zi), 2) );
 
+		    if (distance <= 4.5)
+		    {
+		    	// storing Protein Data
+		    	for (temp = 0; temp < 5; temp++)
+		    		atom_number_p[temp] = p_array[j][temp + 6];
+		    	atom_number_p[5] = '\0';
+		    	int_atom_number_p = atoi(atom_number_p);
+
+		    	for (temp = 0; temp < 3; temp++)
+		    		atom_name_p[temp] = p_array[j][temp + 13];
+		    	atom_name_p[3] = '\0';
+		    	
+		    	for (temp = 0; temp < 3; temp++)
+		    		residue_name_p[temp] = p_array[j][temp + 17];
+		    	residue_name_p[3] = '\0';
+
+		    	for (temp = 0; temp < 4; temp++)
+		    		residue_num_p[temp] = p_array[j][temp + 23];
+		    	residue_num_p[4] = '\0';
+		    	int_residue_num_p = atoi(residue_num_p);
+
+		    	// Storing RNA Data
+		    	for (temp = 0; temp < 5; temp++)
+		    		atom_number_r[temp] = r_array[i][temp + 6];
+		    	atom_number_r[5] = '\0';
+		    	int_atom_number_r = atoi(atom_number_r);
+
+		    	for (temp = 0; temp < 3; temp++)
+		    		atom_name_r[temp] = r_array[i][temp + 13];
+		    	atom_name_r[3] = '\0';
+		    	
+		    	for (temp = 0; temp < 3; temp++)
+		    		residue_name_r[temp] = r_array[i][temp + 17];
+		    	residue_name_r[3] = '\0';
+
+		    	for (temp = 0; temp < 4; temp++)
+		    		residue_num_r[temp] = r_array[i][temp + 23];
+		    	residue_num_r[4] = '\0';
+		    	int_residue_num_r = atoi(residue_num_r);
+
+		    	// Store in array_interacting
+		    	fprintf(fpt4, "PROTEIN  %5d  %s %s %4d RNA  %5d  %s %s %4d \n", 
+		    		int_atom_number_p, atom_name_p, residue_name_p, int_residue_num_p, 
+		    		int_atom_number_r, atom_name_r, residue_name_r, int_residue_num_r);
+
+		    	// Store in interacting Protein and RNA arrays
+		    	fprintf(fpt2, "%s\n", p_array[j]);
+		    	fprintf(fpt3, "%s\n", r_array[i]);
+		    	count_interact++;
+		    }
 		}
 	}
 
-	printf("p_counter: %d\n", p_counter); //  DEBUGGER
+	printf("%d\n", count_interact);
 
+	fclose(fpt1);
+	fclose(fpt2);
+	fclose(fpt3);
 
 	return 0;
 }
